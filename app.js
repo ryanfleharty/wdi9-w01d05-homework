c5feb6a97aea861c26edb4d35f2041e7d383b4c2
Jack Bogart <bogart.jack98@gmail.com>
2018-09-17 12:15:54 -0600

    Commit 18 - What's in your Closet

--- app.js
+++ app.js
@@ -197,3 +197,45 @@
 	else {
 		console.log("Monkey");
 	}
+
+//Q18 What's in Your Closet
+
+	const kristynsCloset = [
+	  "left shoe",
+	  "cowboy boots",
+	  "right sock",
+	  "GA hoodie",
+	  "green pants",
+	  "yellow knit hat",
+	  "marshmallow peeps"
+	];
+
+	// Thom's closet is more complicated. Check out this nested data structure!!
+	const thomsCloset = [
+	  [
+	    // These are Thom's shirts
+	    "grey button-up",
+	    "dark grey button-up",
+	    "light blue button-up",
+	    "blue button-up",
+	  ],[
+	    // These are Thom's pants
+	    "grey jeans",
+	    "jeans",
+	    "PJs"
+	  ],[
+	    // Thom's accessories
+	    "wool mittens",
+	    "wool scarf",
+	    "raybans"
+	  ]
+	];
+
+	console.log(`Kristyn is logging that ${kristynsCloset[2]} today`);
+	kristynsCloset.splice(-1, 0, "raybans");
+	console.log(kristynsCloset);
+	kristynsCloset[kristynsCloset.length-3] = "stained knit hat";
+	console.log(kristynsCloset);
+	const outfit = [thomsCloset[0][0], thomsCloset[1][0], thomsCloset[2][0]];
+	console.log(`Thom is looking fiernce in a ${outfit[0]}, ${outfit[1]}, and ${outfit[2]}`);
+	thomsCloset[1][2] = "Footie Pajamas";
