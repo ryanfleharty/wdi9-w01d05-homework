15ed0a40338d80ece6d0f2f48f9b07376f9af7a2
Jack Bogart <bogart.jack98@gmail.com>
2018-09-17 09:25:15 -0600

    commit 1 - Q + A

---
+++ app.js
@@ -0,0 +1,8 @@
+//Q1.1 We use the assignment operator = after or while declaring it 
+//Q1.2 We use the assignment operator again
+//Q1.3 Again, using the asssignment operator
+//Q1.4 Declare is when you create a name for some block of memory to store a value, assigning gives
+//	this memory value, defining does both at the same time
+//Q1.5 Pseudocode is half code half english that helps you plan for your code
+//Q1.6 75% planning 25% coding
+//
