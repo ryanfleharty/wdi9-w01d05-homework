5bccbb46f5015b7340aa80ec39d578ace9b74cee
Jack Bogart <bogart.jack98@gmail.com>
2018-09-18 09:11:48 -0600

    Commit 25 - maxOfThree

--- app.js
+++ app.js
@@ -282,3 +282,12 @@
 		return lengths;
 	}
 	console.log(getMultipleLengths(['arr', 'dog', 'skirt']));
+
+//Q25
+
+	const maxOfThree = (num1, num2, num3) => {
+		let max = num1;
+		if (max < num2){ max = num2;}
+		if (max < num3){ max = num3;}
+		return max;
+	}
